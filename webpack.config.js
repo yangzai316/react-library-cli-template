27400d2326d0610c7cfb88e692539db0f3e1ea2e
cunyangliu <cunyangliu@trip.com>
2021-11-01 19:37:00 +0800

    add neaten-tranfer-webpack-plugin
    Signed-off-by: cunyangliu <cunyangliu@trip.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path')
+const path = require('path');
+const NeatenTransferWebpackPlugin = require('neaten-transfer-webpack-plugin')
 
 module.exports = {
     mode: 'production',
@@ -19,6 +20,20 @@ module.exports = {
             ],
         },]
     },
+    plugins: [new NeatenTransferWebpackPlugin({
+        from: [{
+            path: path.resolve(__dirname, './'),
+            name: 'dist'
+        }, {
+            path: path.resolve(__dirname, './'),
+            name: 'package.json'
+        }],
+        to: {
+            path: path.resolve(__dirname, './example/'),
+            name: 'hello',
+            cover: true
+        }
+    })],
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'bundle.js',
